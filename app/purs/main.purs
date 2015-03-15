22114b9beedbbdf778d263161e02e3acc2153bae
Pascal Hartig <phartig@twitter.com>
2015-03-15 14:12:04 +0000

    A working hello world with Thermite

--- app/purs/main.purs
+++ app/purs/main.purs
@@ -1,5 +1,49 @@
 module Main where
 
 import Debug.Trace
+import Data.Maybe (Maybe(..))
 
-main = trace "Hello, World"
+import qualified Thermite as T
+import qualified Thermite.Html as T
+import qualified Thermite.Html.Elements as T
+import qualified Thermite.Html.Attributes as A
+import qualified Thermite.Events as T
+import qualified Thermite.Action as T
+import qualified Thermite.Types as T
+
+type Tag = String
+
+data Entry = Entry { uri :: String -- TODO: URI
+                   , tags :: [Tag]
+                   , date :: Number -- TODO: DateTime
+                   }
+
+data State = State { entries :: [Entry] -- ^ All entries matching the tag
+                   , tag :: Maybe Tag -- ^ Currently selected tag, if any
+                   }
+
+data Action =
+    NoOp
+
+initialState :: State
+initialState = State { entries: [], tag: Nothing }
+
+spec :: T.Spec _ State _ Action
+spec = T.Spec { initialState: initialState
+              , performAction: performAction
+              , render: render
+              , componentWillMount: Nothing
+              , displayName: Just "giflib-app"
+              }
+
+performAction :: T.PerformAction _ Action (T.Action _ State)
+performAction _ action = T.modifyState (updateState action)
+    where
+    updateState :: Action -> State -> State
+    updateState NoOp = id
+
+render :: T.Render State _ Action
+render ctx (State st) _ =
+    T.div [ A.className "hello-world" ] [ T.text "hello" ]
+
+main = T.render (T.createClass spec) {}
