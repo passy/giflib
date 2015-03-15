4a72e62926992eb76082752fe94df960a82bd7e1
Pascal Hartig <phartig@twitter.com>
2015-03-15 01:37:39 +0000

    Add a pinch of WSK

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,16 @@ var purescript = require('gulp-purescript');
 
 gulp.task('build', function () {
     gulp.src([
-        'purs/*.purs',
+        'app/purs/*.purs',
         'bower_components/purescript-*/src/**/*.purs',
     ]).pipe(purescript.psc({
         output: 'app.js',
         main: true
     }))
-    .pipe(gulp.dest('js/'));
+    .pipe(gulp.dest('app/js/'));
 });
 
-gulp.task('clean', del.bind(null, 'js'));
+gulp.task('clean', del.bind(null, 'js/app.js'));
 
 gulp.task('watch', function () {
     gulp.watch('purs/*.purs', ['build']);
