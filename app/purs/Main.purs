9f0de932f470a5c65dfe9630925a7a42f27cb595
Pascal Hartig <phartig@twitter.com>
2015-03-26 22:03:26 +0000

    First move to Halogen

--- app/purs/Main.purs
+++ app/purs/Main.purs
@@ -5,20 +5,18 @@ import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Maybe.Unsafe (fromJust)
 import Data.Array (map, concat)
 import Data.String (joinWith)
+import Data.Tuple (Tuple(..))
+
+import Halogen (runUI, pureUI)
+import Halogen.Signal (SF1(..), stateful)
+import qualified Halogen.HTML as H
+import qualified Halogen.HTML.Attributes as A
+import qualified Halogen.HTML.Events as A
+import qualified Data.Date as Date
 
 import Web.Giflib.Types (URI(), Tag(), Entry(..))
 import Control.Monad.Eff.DOM (querySelector, appendChild)
 import Control.Monad.Eff.Exception (error, throwException, Exception(..))
-
-import qualified Thermite as T
-import qualified Thermite.Html as T
-import qualified Thermite.Html.Elements as T
-import qualified Thermite.Html.Attributes as A
-import qualified Thermite.Html.Attributes.Extra as A
-import qualified Thermite.Events as T
-import qualified Thermite.Action as T
-import qualified Thermite.Types as T
-import qualified Data.Date as Date
 
 
 data State = State { entries :: [Entry] -- ^ All entries matching the tag
@@ -47,59 +45,21 @@ demoEntries = [ { id: "CDF20EF7-A181-47B7-AB6B-5E0B994F6176"
 demoState :: State
 demoState = State { entries: demoEntries , tag: Nothing }
 
-spec :: T.Spec _ State _ Action
-spec = T.Spec { initialState: demoState
-              , performAction: performAction
-              , render: render
-              , componentWillMount: Nothing
-              , displayName: Just "giflib-app"
-              }
+update :: State -> Action -> State
+update s a = updateState a s
+  where
+  updateState NoOp = id
 
-performAction :: T.PerformAction _ Action (T.Action _ State)
-performAction _ action = T.modifyState (updateState action)
-    where
-    updateState :: Action -> State -> State
-    updateState NoOp = id
+view :: forall p r node. (H.HTMLRepr node) => SF1 Action (node p r)
+view = render <$> stateful demoState update
+  where
+  render :: State -> node p r
+  render _ =
+    H.p_ [ H.text "Hello, World" ]
 
-
-render :: T.Render State _ Action
-render ctx (State st) _ =
-    T.div [ A.className "giflib-app" ]
-        [ T.div [ A.className "gla-card-holder" ] $ map entryCard st.entries
-        ]
-
-    where
-
-    entryCard :: Entry -> T.Html _
-    entryCard e = T.div
-        [ A.className "wsk-card wsk-shadow--z3"
-        , A.key e.id
-        ]
-        [ T.div [ A.className "wsk-card--img-container"
-                , A.style $ { "backgroundImage": "url(" ++ e.uri ++ ")" }
-                ] []
-        , T.div [ A.className "wsk-card--heading" ]
-            [ T.h2
-                [ A.className "wsk-card--heading-text" ] [ T.text $ formatEntryTags e ]
-            ]
-        , T.div [ A.className "wsk-card--caption" ] [ T.text $ formatEntryDatetime e ]
-        , T.div [ A.className "wsk-card--bottom" ]
-            [ T.a
-                [ A.href e.uri
-                , A.className "wsk-card--uri"
-                , A.target "_blank" ] [ T.text e.uri ]
-            ]
-        ]
-
-formatEntryDatetime :: forall e. { date :: Date.Date | e } -> String
-formatEntryDatetime e = show e.date
-
-formatEntryTags :: forall e. { tags :: [Tag] | e } -> String
-formatEntryTags e = joinWith " " $ map (\x -> "#" ++ x) e.tags
-
-main :: forall eff. Control.Monad.Eff.Eff (dom :: DOM.DOM, err :: Exception | eff) Unit
 main = do
-    el <- querySelector "#app-main"
-    case el of
-         Just e -> T.renderTo e (T.createClass spec) {}
-         Nothing -> throwException $ error "Couldn't find #app-main. What've you done to the HTML?"
+  Tuple node driver <- runUI (pureUI view)
+  el <- querySelector "#app-main"
+  case el of
+    Just e -> appendChild el node
+    Nothing -> throwException $ error "Couldn't find #app-main. What've you done to the HTML?"
