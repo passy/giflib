fc5ea2bc0f5711e0445b0c2afa4fa0b6b62961b3
Pascal Hartig <phartig@twitter.com>
2015-04-19 21:52:39 +0200

    Docstring for mip

--- app/purs/Web/WSK/Internal.purs
+++ app/purs/Web/WSK/Internal.purs
@@ -3,5 +3,10 @@ module WSK.Internal where
 import Data.Maybe
 import Data.Monoid
 
+-- | Apply a function on a Maybe value inside a monoid or convert to an empty
+-- monoid instead. Useful for optional attributes to elements. Don't ask me
+-- what the name stands for. It's short.
+-- > button t = H.button ([ A.classes btnClasses ] <>
+-- >                      (mip A.id_ t.id)) [ H.text t.text ]
 mip :: forall a b f. (Monoid (f b), Applicative f) => (a -> b) -> Maybe a -> f b
 mip f = maybe mempty (pure <<< f)
