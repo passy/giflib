d0f7ca4ac57ee8b63ecc645271c8b38854515b4b
Pascal Hartig <phartig@twitter.com>
2015-04-25 17:17:19 +0100

    Fix the build

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,30 @@ var swallowError = function (error) {
 };
 
 var build = function (errorHandler) {
+    var pscMake = purescript.psc({
+        main: true,
+        noOpts: false,
+        noMagicDo: false,
+        output: 'app.js'
+    });
+
+    pscMake.on('error', function(e) {
+        console.error(e.message);
+        pscMake.end();
+    });
+
     return gulp.src([
         'app/purs/*.purs',
         'app/purs/**/*.purs',
         'bower_components/purescript-*/src/**/*.purs',
-    ]).pipe(purescript.pscMake({
-        output: 'tmp',
-        main: true,
-        noOpts: true,
-        noMagicDo: true
-    }))
-    .on('error', errorHandler)
+    ]).pipe(pscMake)
     .pipe(browserify({}))
     .pipe(gulp.dest('app/js/'));
 };
 
 gulp.task('build', build.bind(null, swallowError));
 
-gulp.task('clean', del.bind(null, ['tmp', 'js/app.js']));
+gulp.task('clean', del.bind(null, ['tmp', 'purs/js/app.js']));
 
 gulp.task('watch', function () {
     gulp.watch(['app/purs/*.purs', 'app/purs/**/*.purs'], ['build']);
