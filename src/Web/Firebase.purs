30d52b9e7ba396497cba3eb497ada29ed05ae23c
Pascal Hartig <phartig@twitter.com>
2015-04-26 12:46:04 +0100

    Use Fn1 instead of manual closures

--- src/Web/Firebase.purs
+++ src/Web/Firebase.purs
@@ -1,17 +1,24 @@
-module Web.Firebase where
+module Web.Firebase
+( newFirebase
+, child
+, printVals
+)
+where
 
 import Control.Monad.Eff (Eff())
 import Web.Firebase.Types (Firebase(), FirebaseEff())
 import Halogen.HTML.Target (URL())
+import Data.Function (Fn1(), runFn1)
 
 
-foreign import newFirebase """
+foreign import newFirebaseImpl """
   function newFirebase(uri) {
-    return function () {
-      return new Firebase(uri);
-    };
+    return new Firebase(uri);
   }
-""" :: forall eff. URL -> Eff (firebase :: FirebaseEff | eff) Firebase
+""" :: forall eff. Fn1 URL (Eff (firebase :: FirebaseEff | eff) Firebase)
+
+newFirebase :: forall eff. URL -> Eff (firebase :: FirebaseEff | eff) Firebase
+newFirebase = runFn1 newFirebaseImpl
 
 foreign import child """
   function child(childPath) {
