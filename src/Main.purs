90fcc574bf1312a191c7bee2c3cf8252a62f3e4e
Pascal Hartig <phartig@twitter.com>
2015-08-10 21:12:52 +0100

    Reintroduce some debugging hooks

--- src/Main.purs
+++ src/Main.purs
@@ -56,6 +56,8 @@ import qualified Web.Firebase.DataSnapshot as DS
 import qualified Web.Firebase.Types as FB
 import qualified Data.Foreign as Foreign
 
+import Web.Giflib.Internal.Unsafe
+
 import Web.Giflib.Types (Tag(), Entry(..), uuid, runUUID)
 
 data LoadingStatus
@@ -117,7 +119,7 @@ update s' a = updateState a s'
                                         , newTags = (Set.empty :: Set.Set Tag)
                                         }
   updateState (LoadingAction l a) s = updateState a $ s { loadingStatus = l }
-  updateState (UpdateNewURL e) s    = s { newUrl  = e }
+  updateState (UpdateNewURL e) s    = s { newUrl  = unsafeShowPrintId $ e }
   updateState (UpdateNewTags e) s   = s { newTags = processTagInput e }
   updateState (UpdateEntries e) s   = s { entries = e }
   updateState (ShowError e) s       = s { error   = e }
@@ -139,7 +141,7 @@ handler (AppConfig conf) (AddNewEntry s) = do
   -- TODO: Would be nice to have a MonadReader/ReaderT for this. Like, really
   -- nice. But I'm too stupid.
   children <- liftEff $ FB.child "entries" conf.firebase
-  liftEff $ FB.push (Foreign.toForeign $ encodeJson entry) Nothing children
+  liftEff $ FB.push (Foreign.toForeign $ unsafeShowPrintId $ encodeJson entry) Nothing children
   E.yield ResetNewForm
 
 ui :: forall eff. AppConfig -> Component (E.Event (AppEff eff)) Action Action
