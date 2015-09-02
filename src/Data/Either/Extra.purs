26dc2275ec171ec229ef8559a4bcccc236de3c1e
Pascal Hartig <phartig@twitter.com>
2015-09-02 23:11:15 +0200

    rightToMaybe combinator

--- src/Data/Either/Extra.purs
+++ src/Data/Either/Extra.purs
@@ -4,7 +4,7 @@ module Data.Either.Extra
 
 import Prelude
 import Data.Either (Either(), either)
-import Data.Maybe
+import Data.Maybe (Maybe(Just, Nothing))
 
 rightToMaybe :: forall a b. Either a b -> Maybe b
-rightToMaybe = either (\_ -> Nothing) Just
+rightToMaybe = either (const Nothing) Just
